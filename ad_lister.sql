b034fc29a3c761138ee70d6f4d1ff06a7a0c5dff
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-03-06 15:56:35 -0600

    quiries for ad lister

--- ad_lister.sql
+++ ad_lister.sql
@@ -37,6 +37,30 @@ CREATE TABLE ads_categories (
 );
 
 
+#For a given ad, what is the email address of the user that created it?
+select u.email
+from users u
+inner join  ads a on u.id = a.user_id
+where a.id = 2;
+
+# For a given ad, what category, or categories, does it belong to?
+select c.category
+from categories c
+inner join ads_categories ac on c.id = ac.category_id
+where ac.ad_id = 2;
+
+# For a given category, show all the ads that are in that category.
+select a.*
+from ads a
+inner join ads_categories ac on a.id = ac.ad_id
+inner join categories c on ac.category_id = c.id
+where c.category like '%pets%';
+
+# For a given user, show all the ads they have posted.
+select a.*
+from ads as a
+where a.user_id = 3;
+
 
 
 
