b17c43aee707795103775914852a81f9e6fa5230
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-03-08 15:50:51 -0600

    subqueries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,21 +1,21 @@
-# USE ymir_employees;
-#
-# select e2.hire_date
-# from employees
-# where e.hire_date in (
-#     select e2.hire_date
-#     from employees e2
-#     where e2.emp_no = '101010'
-#     );
-#
-# select distinct t.*
-# from titles t where e.emp_no in (
-#     select e2.emp_no
-#     from employees e2
-#     where e2.first_name = 'Aamond'
-#     );
+use ymir_employees;
 
+select * from employees e
+where e.hire_date in (select emps.hire_date
+                      from employees emps
+                      where emps.emp_no = 101010);
 
-select dm2.emp_no
-from dept_manager dm2
-where dm2.to_date = '999-01-01'
+select title
+from titles t
+where emp_no
+in (select emp_no from employees where first_name = 'Aamod');
+
+select distinct title
+from titles t
+where emp_no
+in (select emp_no from employees where first_name = 'Aamod');
+
+select first_name, last_name
+from employees e
+where e.emp_no
+in (select dm.emp_no from dept_manager dm where dm.to_date = '9999-01-01' and e.gender = 'F');
